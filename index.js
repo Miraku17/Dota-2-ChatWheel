e1a831993287d7561358578a3a0a9c819d24359a
Zian Rinzler Valles <83147228+Miraku17@users.noreply.github.com>
2023-02-05 21:20:29 +0800

    Updated
    No working keyboard click

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-
-
 var numberOfDrums = document.querySelectorAll(".drum").length;
 
-
 for (let index = 0; index < numberOfDrums; index++) {
-    
-     document.querySelectorAll(".drum")[index].addEventListener("click",function(){
-            
-            if(index===0){
-                var perfect = new Audio("sounds2/perfect.mp3");
-                perfect.play();
-            }else if(index===1){
-                var next = new Audio("sounds2/next-level-play.mp3");
-                next.play();
-            }
-            else if(index===2){
-                var ceb = new Audio("sounds2/ceb.mp3");
-                ceb.play();
-            }
-     });
+  document
+    .querySelectorAll(".drum")
+    [index].addEventListener("click", function () {
+      // if(index===0){
+      //     var perfect = new Audio("sounds2/perfect.mp3");
+      //     perfect.play();
+      // }else if(index===1){
+      //     var next = new Audio("sounds2/next-level-play.mp3");
+      //     next.play();
+      // }
+      // else if(index===2){
+      //     var ceb = new Audio("sounds2/ceb.mp3");
+      //     ceb.play();
+      // }
 
- 
+      switch (index) {
+        case 0:
+          var perfect = new Audio("sounds2/perfect.mp3");
+          perfect.play();
+          break;
+        case 1:
+          var next = new Audio("sounds2/next-level-play.mp3");
+          next.play();
+          break;
+        case 2:
+          var ceb = new Audio("sounds2/ceb.mp3");
+          ceb.play();
+          break;
+        case 3:
+          var ding = new Audio("sounds2/ding.mp3");
+          ding.play();
+          break;
+        case 4:
+          var eyona = new Audio("sounds2/eyona.mp3");
+          eyona.play();
+          break;
+        case 5:
+          var echo = new Audio("sounds2/echoslam.mp3");
+          echo.play();
+          break;
+        case 6:
+          var magic = new Audio("sounds2/magic.mp3");
+          magic.play();
+          break;
+      }
+
+      this.style.color = "green";
+    });
 }
