fe724302c3a01fb4642d24af1147ed3ac65f6754
Zian Rinzler Valles <83147228+Miraku17@users.noreply.github.com>
2023-02-06 20:36:18 +0800

    With Animation

--- index.js
+++ index.js
@@ -1,52 +1,67 @@
 var numberOfDrums = document.querySelectorAll(".drum").length;
 
 for (let index = 0; index < numberOfDrums; index++) {
-  document
-    .querySelectorAll(".drum")
-    [index].addEventListener("click", function () {
-      // if(index===0){
-      //     var perfect = new Audio("sounds2/perfect.mp3");
-      //     perfect.play();
-      // }else if(index===1){
-      //     var next = new Audio("sounds2/next-level-play.mp3");
-      //     next.play();
-      // }
-      // else if(index===2){
-      //     var ceb = new Audio("sounds2/ceb.mp3");
-      //     ceb.play();
-      // }
+  document.querySelectorAll(".drum")[index].addEventListener("click", function () {
 
-      switch (index) {
-        case 0:
+
+        var buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+
+    });
+
+
+}
+
+document.addEventListener("keypress",function(event){
+
+        makeSound(event.key);
+        buttonAnimation(event.key);
+});
+
+
+function makeSound(key){
+    switch (key) {
+        case "w":
           var perfect = new Audio("sounds2/perfect.mp3");
           perfect.play();
           break;
-        case 1:
+        case "a":
           var next = new Audio("sounds2/next-level-play.mp3");
           next.play();
           break;
-        case 2:
+        case "s":
           var ceb = new Audio("sounds2/ceb.mp3");
           ceb.play();
           break;
-        case 3:
+        case "d":
           var ding = new Audio("sounds2/ding.mp3");
           ding.play();
           break;
-        case 4:
+        case "j":
           var eyona = new Audio("sounds2/eyona.mp3");
           eyona.play();
           break;
-        case 5:
+        case "k":
           var echo = new Audio("sounds2/echoslam.mp3");
           echo.play();
           break;
-        case 6:
+        case "l":
           var magic = new Audio("sounds2/magic.mp3");
           magic.play();
           break;
       }
+}
 
-      this.style.color = "green";
-    });
+function buttonAnimation(key){
+
+  var active = document.querySelector("."+ key);
+
+  active.classList.add("pressed");
+
+  setTimeout(function(){
+    active.classList.remove("pressed");
+  },100);
 }
